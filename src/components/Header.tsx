37523a983cc40c264dfe09502bacc6371b9708f6
Valenmar03 <valenmrtnz8@gmail.com>
2025-01-02 16:53:16 -0300

    Unifico headers

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,129 +1,101 @@
 import { useState, useRef, useEffect } from "react";
 
 export default function Header() {
-   const [menuOpen, setMenuOpen] = useState(false);
-   const [menuHeight, setMenuHeight] = useState("0px");
-   const navRef = useRef<HTMLDivElement>(null);
-
-   useEffect(() => {
-      if (menuOpen) {
-         setMenuHeight(`${navRef.current!.scrollHeight}px`);
-      } else {
-         setMenuHeight("0px");
-      }
-   }, [menuOpen]);
+   const phoneNumber = "+5491159606813";
+   const defaultMessage = encodeURIComponent(
+      "Hola! Me gustaría recibir más información sobre sus productos."
+   );
+   const whatsappLink = `https://wa.me/${phoneNumber}?text=${defaultMessage}`;
+   const [openNav, setOpenNav] = useState(false);
 
    return (
       <header className="bg-white shadow-md fixed top-0 w-full z-50">
-         <div className="container mx-auto flex flex-col md:flex-row justify-between md:justify-around  items-center py-4 px-6">
-            <div className="flex justify-between items-center w-full md:block md:w-fit">
-               <a href="/" className="text-2xl font-bold text-green-600">
-                  <img
-                     src="/LogoTexto.png"
-                     alt="Logo Ecorganico"
-                     className="w-44 hidden md:block"
-                     loading="lazy"
-                  />
-                  <img
-                     src="/Logo.png"
-                     alt="Logo Ecorganico"
-                     className="w-14 md:hidden"
-                     loading="lazy"
-
-                  />
-               </a>
-               <button
-                  className="md:hidden"
-                  onClick={() => setMenuOpen(!menuOpen)}
+         <div className="container mx-auto flex flex-col md:flex-row justify-between md:justify-around items-center py-4 px-6">
+            {/* Logo */}
+            <nav className="flex flex-col gap-5 md:flex-row justify-between md:items-center w-full">
+               <div className="flex justify-between">
+                  <a href="/" className="text-2xl font-bold text-green-600">
+                     <img
+                        src="/LogoTexto.png"
+                        alt="Logo Ecorganico"
+                        className="w-44 hidden md:block"
+                        loading="lazy"
+                     />
+                     <img
+                        src="/Logo.png"
+                        alt="Logo Ecorganico"
+                        className="w-14 md:hidden"
+                        loading="lazy"
+                     />
+                  </a>
+                  <button
+                     className="md:hidden"
+                     onClick={() => setOpenNav(!openNav)}
+                  >
+                     <img src="/svg/Menu.svg" alt="Menu svg" className="w-8" />
+                  </button>
+               </div>
+               <div
+                  className={`${
+                     openNav ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
+                  } transition-all duration-500 ease-in-out overflow-hidden md:overflow-visible md:max-h-none md:opacity-100 md:flex md:gap-10 md:items-center w-full justify-center text-xl`}
                >
-                  <img src="/svg/Menu.svg" alt="Menu svg" className="w-8" />
-               </button>
-            </div>
-
-            <nav
-               ref={navRef}
-               style={{ maxHeight: menuHeight }}
-               className={`md:hidden overflow-hidden transition-[max-height] duration-500 ease-in-out w-full mt-5 md:mt-0`}
-            >
-               <ul className="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-6 text-gray-600 text-2xl ">
-                  <li>
-                     <a
-                        href="#inicio"
-                        className="hover:text-green-500 duration-200 p-3"
-                     >
-                        Inicio
-                     </a>
-                  </li>
-                  <li>
-                     <a
-                        href="#nosotros"
-                        className="hover:text-green-500 duration-200 p-3"
-                     >
-                        Nosotros
-                     </a>
-                  </li>
-                  <li>
-                     <a
-                        href="#productos"
-                        className="hover:text-green-500 duration-200 p-3"
-                     >
-                        Productos
-                     </a>
-                  </li>
-                  <li>
-                     <a
-                        href="#contacto"
-                        className="hover:text-green-500 duration-200 p-3"
-                     >
-                        Contacto
-                     </a>
-                  </li>
-               </ul>
+                  <ul className="flex flex-col gap-3 md:flex-row md:gap-10 md:text-lg text-gray-700 mb-5 md:mb-0">
+                     <li>
+                        <a
+                           href="#inicio"
+                           className="hover:text-green-500 duration-200"
+                        >
+                           Inicio
+                        </a>
+                     </li>
+                     <li>
+                        <a
+                           href="#nosotros"
+                           className="hover:text-green-500 duration-200"
+                        >
+                           Nosotros
+                        </a>
+                     </li>
+                     <li>
+                        <a
+                           href="#productos"
+                           className="hover:text-green-500 duration-200"
+                        >
+                           Productos
+                        </a>
+                     </li>
+                     <li>
+                        <a
+                           href="#contacto"
+                           className="hover:text-green-500 duration-200"
+                        >
+                           Contacto
+                        </a>
+                     </li>
+                  </ul>
+                  <a
+                     href={whatsappLink}
+                     target="_blank"
+                     className="bg-green-200 md:hidden justify-center text-green-900 px-4 md:px-10 py-2 rounded hover:bg-green-300 duration-200 flex gap-2 items-center"
+                  >
+                     <img
+                        src="/svg/Wpp.svg"
+                        alt="WhatsApp Svg"
+                        className="w-8"
+                     />
+                     WhatsApp
+                  </a>
+               </div>
+               <a
+                  href={whatsappLink}
+                  target="_blank"
+                  className="bg-green-200 hidden md:flex text-green-900 px-4 md:px-10 py-2 rounded hover:bg-green-300 duration-200 gap-2 items-center justify-center"
+               >
+                  <img src="/svg/Wpp.svg" alt="WhatsApp Svg" className="w-8" />
+                  WhatsApp
+               </a>
             </nav>
-
-            <nav className="hidden md:block">
-               <ul className="flex space-x-6 text-gray-600 text-lg">
-                  <li>
-                     <a
-                        href="#inicio"
-                        className="hover:text-green-500 duration-200"
-                     >
-                        Inicio
-                     </a>
-                  </li>
-                  <li>
-                     <a
-                        href="#nosotros"
-                        className="hover:text-green-500 duration-200"
-                     >
-                        Nosotros
-                     </a>
-                  </li>
-                  <li>
-                     <a
-                        href="#productos"
-                        className="hover:text-green-500 duration-200"
-                     >
-                        Productos
-                     </a>
-                  </li>
-                  <li>
-                     <a
-                        href="#contacto"
-                        className="hover:text-green-500 duration-200"
-                     >
-                        Contacto
-                     </a>
-                  </li>
-               </ul>
-            </nav>
-
-            <a
-               href="#contacto"
-               className="bg-green-200 text-green-900 px-4 py-2 rounded hover:bg-green-300 duration-200 hidden md:block"
-            >
-               Solicitá Información
-            </a>
          </div>
       </header>
    );
--- src/components/HeaderProductsPage.tsx
+++ src/components/HeaderProductsPage.tsx
@@ -1,5 +1,5 @@
 import type { InferEntrySchema, RenderedContent } from "astro:content";
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 
 type HeaderProductsPageProps = {
    products: {
@@ -19,178 +19,71 @@ export default function HeaderProductsPage({
       "Hola! Me gustaría recibir más información sobre sus productos."
    );
    const whatsappLink = `https://wa.me/${phoneNumber}?text=${defaultMessage}`;
-   const [menuOpen, setMenuOpen] = useState(false);
-   const [summaryPressed, setSummaryPressed] = useState(false);
-   const [menuHeight, setMenuHeight] = useState("0px");
-   const navRef = useRef<HTMLDivElement>(null);
-
-   useEffect(() => {
-      if (menuOpen) {
-         setMenuHeight(`h-screen`);
-      } else {
-         setMenuHeight("h-0");
-      }
-   }, [menuOpen, summaryPressed]);
+   const [openNav, setOpenNav] = useState(false);
 
    return (
       <header className="bg-white shadow-md fixed top-0 w-full z-50">
          <div className="container mx-auto flex flex-col md:flex-row justify-between md:justify-around items-center py-4 px-6">
             {/* Logo */}
-            <div className="flex justify-between items-center w-full md:block md:w-fit">
-               <a href="/" className="text-2xl font-bold text-green-600">
-                  <img
-                     src="/LogoTexto.png"
-                     alt="Logo Ecorganico"
-                     className="w-44 hidden md:block"
-                     loading="lazy"
-                  />
-                  <img
-                     src="/Logo.png"
-                     alt="Logo Ecorganico"
-                     className="w-14 md:hidden"
-                     loading="lazy"
-                  />
-               </a>
-               <button
-                  className="md:hidden"
-                  onClick={() => setMenuOpen(!menuOpen)}
+            <nav className="flex flex-col gap-5 md:flex-row justify-between md:items-center w-full">
+               <div className="flex justify-between">
+                  <a href="/" className="text-2xl font-bold text-green-600">
+                     <img
+                        src="/LogoTexto.png"
+                        alt="Logo Ecorganico"
+                        className="w-44 hidden md:block"
+                        loading="lazy"
+                     />
+                     <img
+                        src="/Logo.png"
+                        alt="Logo Ecorganico"
+                        className="w-14 md:hidden"
+                        loading="lazy"
+                     />
+                  </a>
+                  <button
+                     className="md:hidden"
+                     onClick={() => setOpenNav(!openNav)}
+                  >
+                     <img src="/svg/Menu.svg" alt="Menu svg" className="w-8" />
+                  </button>
+               </div>
+               <div
+                  className={`${
+                     openNav ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
+                  } transition-all duration-500 ease-in-out overflow-hidden md:overflow-visible md:max-h-none md:opacity-100 md:flex md:gap-10 md:items-center w-full justify-center text-xl`}
                >
-                  <img src="/svg/Menu.svg" alt="Menu svg" className="w-8" />
-               </button>
-            </div>
-
-            <nav
-               className={`md:hidden overflow-hidden ${
-                  menuOpen ? "h-screen" : "h-0"
-               } transition-all duration-500 ease-in-out w-full mt-5 md:mt-0`}
-            >
-               <ul className="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-6 text-gray-600 text-2xl">
-                  <li>
-                     <a
-                        href="/"
-                        className="hover:text-green-500 duration-200 p-3"
-                     >
-                        Inicio
-                     </a>
-                  </li>
-                  <li>
-                     <details
-                        className="p-3 group"
-                        onClick={() => setSummaryPressed(!summaryPressed)}
-                     >
-                        <summary className="hover:text-green-500 list-none flex justify-between">
-                           DiatomiD
-                           <img
-                              src="/svg/ChevronDown.svg"
-                              alt="Chevron Icon"
-                              className="-rotate-90 group-open:rotate-0 duration-200"
-                           />
-                        </summary>
-                        <ul className="text-lg ml-5">
-                           {products
-                              .filter(
-                                 (product) =>
-                                    product.data.supplier === "DiatomiD" &&
-                                    product
-                              )
-                              .map((product) => (
-                                 <li
-                                    key={product.id}
-                                    className="hover:text-green-500"
-                                 >
-                                    <a href={`/product/${product.id}`}>
-                                       {product.data.title}
-                                    </a>
-                                 </li>
-                              ))}
-                        </ul>
-                     </details>
-                  </li>
-                  <li>
-                     <details
-                        className="p-3 group"
-                        onClick={() => setSummaryPressed(!summaryPressed)}
-                     >
-                        <summary className="hover:text-green-500 list-none flex justify-between">
-                           Nutrigea
-                           <img
-                              src="/svg/ChevronDown.svg"
-                              alt="Chevron Icon"
-                              className="-rotate-90 group-open:rotate-0 duration-200"
-                           />
-                        </summary>
-                        <ul className="text-lg ml-5">
-                           {products
-                              .filter(
-                                 (product) =>
-                                    product.data.supplier === "DiatomiD" &&
-                                    product
-                              )
-                              .map((product) => (
-                                 <li
-                                    key={product.id}
-                                    className="hover:text-green-500"
-                                 >
-                                    <a href={`/product/${product.id}`}>
-                                       {product.data.title}
-                                    </a>
-                                 </li>
-                              ))}
-                        </ul>
-                     </details>
-                  </li>
-                  <li>
-                     <a
-                        href={whatsappLink}
-                        target="_blank"
-                        className="bg-green-200 text-green-900 px-4 py-2 rounded hover:bg-green-300 duration-200 flex gap-2 items-center"
-                     >
-                        <img
-                           src="/svg/Wpp.svg"
-                           alt="WhatsApp Svg"
-                           className="w-8"
-                        />
-                        WhatsApp
-                     </a>
-                  </li>
-               </ul>
+                  <ul className="flex flex-col gap-3 md:flex-row md:gap-10 md:text-lg text-gray-700 mb-5 md:mb-0">
+                     <li>
+                        <a href="/" className="hover:text-green-600">
+                           Inicio
+                        </a>
+                     </li>
+                     <li>DiatomiD</li>
+                     <li>Nutrigea</li>
+                  </ul>
+                  <a
+                     href={whatsappLink}
+                     target="_blank"
+                     className="bg-green-200 md:hidden justify-center text-green-900 px-4 md:px-10 py-2 rounded hover:bg-green-300 duration-200 flex gap-2 items-center"
+                  >
+                     <img
+                        src="/svg/Wpp.svg"
+                        alt="WhatsApp Svg"
+                        className="w-8"
+                     />
+                     WhatsApp
+                  </a>
+               </div>
+               <a
+                  href={whatsappLink}
+                  target="_blank"
+                  className="bg-green-200 hidden md:flex text-green-900 px-4 md:px-10 py-2 rounded hover:bg-green-300 duration-200 gap-2 items-center justify-center"
+               >
+                  <img src="/svg/Wpp.svg" alt="WhatsApp Svg" className="w-8" />
+                  WhatsApp
+               </a>
             </nav>
-
-            <nav className="hidden md:block">
-               <ul className="flex space-x-6 text-gray-600 text-lg">
-                  <li>
-                     <a href="/" className="hover:text-green-500 duration-200">
-                        Inicio
-                     </a>
-                  </li>
-                  <li>
-                     <a
-                        href="#nosotros"
-                        className="hover:text-green-500 duration-200 p-3"
-                     >
-                        Diatomid
-                     </a>
-                  </li>
-                  <li>
-                     <a
-                        href="#productos"
-                        className="hover:text-green-500 duration-200 p-3"
-                     >
-                        Nutrigea
-                     </a>
-                  </li>
-               </ul>
-            </nav>
-
-            {/* WhatsApp Link */}
-            <a
-               href={whatsappLink}
-               target="_blank"
-               className="bg-green-200 text-green-900 px-4 py-2 rounded hover:bg-green-300 duration-200 hidden md:flex gap-2 items-center"
-            >
-               <img src="/svg/Wpp.svg" alt="WhatsApp Svg" className="w-8" />
-               WhatsApp
-            </a>
          </div>
       </header>
    );
